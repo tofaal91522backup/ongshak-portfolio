752aebd2e885425830468fbd001aa8a2ff4639dc
tofaal91522backup <tofaal91522backup@gmail.com>
2026-04-20 23:04:58 +0600

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import AppProviders from "@/providers/app-providers";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "This is the title of the page",
+  description:
+    "This is the description of the page. It should be concise and informative, providing a clear overview of the content and purpose of the page.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning suppressContentEditableWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <AppProviders>{children}</AppProviders>
+      </body>
+    </html>
+  );
+}
